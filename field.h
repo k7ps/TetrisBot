411342fa07e5bd75ed02c53ebcc30109495fa32e
k7ps <thek7ps@gmail.com>
2024-05-04 16:18:28 +0300

    Add Field::EraseLastAddedPiece()

--- field.h
+++ field.h
@@ -5,6 +5,26 @@
 #include "point.h"
 
 #include <vector>
+#include <stack>
+
+
+struct Event {
+    enum EventType {
+        ADD_PIECE,
+        CLEAR_LINES
+    };
+
+    Event(PiecePosition piecePos)
+        : Type(ADD_PIECE), AddedPiece(piecePos) {}
+
+    Event(std::vector<int>&& y, std::vector<std::vector<int8_t>>&& lines)
+        : Type(CLEAR_LINES), Heights(std::move(y)), ClearedLines(std::move(lines)) {}
+
+    EventType Type;
+    PiecePosition AddedPiece;
+    std::vector<int> Heights;
+    std::vector<std::vector<int8_t>> ClearedLines;
+};
 
 
 class Field {
@@ -15,28 +35,22 @@ public:
         return Data[index].data(); 
     }        
 
-    Point GetSize() const {
-        return Size;
-    }
-
-    int GetLineCount() const {
-        return LineCount;
-    }
+    Point GetSize() const { return Size; }
+    int GetLineCount() const { return LineCount;}
 
     // return TRUE if action done, FALSE if not
-    bool CanPut(const Point& pos, const Piece& piece) const;
-    bool CanPut(const PiecePosition& piecePos) const;
-    bool Put(const Point& pos, const Piece& piece);
-    bool Put(const PiecePosition& piecePos);
-    bool PutAtStart(PieceType type);
-
-    bool CanErase(const Point& pos, const Piece& piece) const;
-    bool CanErase(const PiecePosition& piecePos) const;
-    bool Erase(const Point& pos, const Piece& piece);
-    bool Erase(const PiecePosition& piecePos);
-    bool EraseFromStart();
+    bool CanPut(const PiecePosition&) const;
+    bool Put(const PiecePosition&);
+    bool PutAndClearFilledLines(const PiecePosition&);
+    bool PutAtStart(PieceType);
 
     bool ClearFilledLines();
+
+    void EraseLastAddedPiece();
+
+private:
+    void Erase(const PiecePosition&);
+    void RestoreClearedLines(const std::vector<int>& heights, const std::vector<std::vector<int8_t>>& lines);
 
 private:
     Point Size;
@@ -46,6 +60,8 @@ private:
     PiecePosition PieceAtStart;
 
     int LineCount;
+
+    std::stack<Event> Events;
 };
 
 #endif // FIELD_H
